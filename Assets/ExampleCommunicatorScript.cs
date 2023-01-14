3f6ba923b2b1bcde25f1a5fcc19f506c62408ee7
HazelDragon <eloise@cyberpunk.com>
2023-01-13 20:46:31 -0500

    updating scene with communicatorscript to interface unity and singularity
    updating scene with communicatorscript to interface unity and singularity, also adding missing file from gradle properties

---
+++ Assets/ExampleCommunicatorScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sngty;
+using UnityEngine;
+
+public class ExampleCommunicatorScript : MonoBehaviour
+{
+    public SingularityManager mySingularityManager;
+    DeviceSignature myDevice;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       List<DeviceSignature> pairedDevices = mySingularityManager.GetPairedDevices();
+       //DeviceSignature myDevice = new DeviceSignature();
+       myDevice = new DeviceSignature();
+
+        //If you are looking for a device with a specific name (in this case exampleDeviceName):
+        for (int i = 0; i < pairedDevices.Count; i++)
+        {
+            if (pairedDevices[i].name == "Kitchen-ESP32")
+            {
+                myDevice = pairedDevices[i];
+                //Do stuff to connect to the device here
+                mySingularityManager.ConnectToDevice(myDevice);
+                break;
+            }
+        }
+
+        if (!myDevice.Equals(default(DeviceSignature)))
+        {
+
+        }
+    }
+
+    public void SendMessageFromInteraction(string message){
+        mySingularityManager.sendMessage(message + "\n", myDevice);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
